7d9354573374dbb567ce91dfafddd776dcb4305c
João Castelli <joaao.castelli@gmail.com>
2020-06-05 17:48:34 -0300

    finished forgot and reset password

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,12 +2,14 @@ import React, { ButtonHTMLAttributes } from 'react';
 
 import { Container } from './styles';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading?: boolean;
+};
 
-const Input: React.FC<ButtonProps> = ({ children, ...rest }) => (
+const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => (
   <Container type="button" {...rest}>
-    {children}
+    {loading ? 'Carregando...' : children}
   </Container>
 );
 
-export default Input;
+export default Button;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,11 +6,16 @@ import Route from './Route';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
+import ForgotPassowrd from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+    <Route path="/forgot-password" component={ForgotPassowrd} />
+    <Route path="/reset-password" component={ResetPassword} />
+
     <Route path="/dashboard" component={Dashboard} />
   </Switch>
 );
